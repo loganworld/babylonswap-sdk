af4e912c05b7f6f0d17ca31b19fb632f8def16bf
loganworld <akp707d@gmail.com>
2022-04-18 06:27:15 +1000

    fix:color

--- src/entities/currency.ts
+++ src/entities/currency.ts
@@ -16,7 +16,7 @@ export class Currency {
   /**
    * The only instance of the base class `Currency`.
    */
-  public static readonly ETHER: Currency = new Currency(18, 'WBNB', 'WBNB')
+  public static readonly ETHER: Currency = new Currency(18, 'BNB', 'Binance coin')
 
   /**
    * Constructs an instance of the base class `Currency`. The only instance of the base class `Currency` is `Currency.ETHER`.
